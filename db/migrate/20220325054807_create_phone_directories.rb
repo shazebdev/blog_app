3fe7529da26b83d1f8ee918c6245ac15f06c75d2
shahzebdev <shahzebrajpoot197@gmail.com>
2022-03-25 11:31:55 +0500

    Mofifying app

--- db/migrate/20220325054807_create_phone_directories.rb
+++ db/migrate/20220325054807_create_phone_directories.rb
@@ -2,7 +2,7 @@ class CreatePhoneDirectories < ActiveRecord::Migration[7.0]
   def change
     create_table :phone_directories do |t|
       t.string :name
-      t.numeric :phone
+      t.text :phone
       t.string :email
 
       t.timestamps
